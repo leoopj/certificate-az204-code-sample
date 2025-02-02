bab130acfe9ca6a2ed67ddd9f1926de57d155fb0
Leonardo Pereira de Jesus <leonardo.pereira@keeggo.com>
2025-02-02 16:17:15 -0300

    Added cache with Redis

---
+++ Cache/Redis/Program.cs
@@ -0,0 +1,28 @@
+﻿using StackExchange.Redis;
+
+string connectionString = "{myConnectionStringRedis}";
+
+using (var cache = ConnectionMultiplexer.Connect(connectionString))
+{
+    IDatabase db = cache.GetDatabase();
+    db.StringSet("key1", "value1");
+
+    string getValue = await db.StringGetAsync("key1");
+    Console.WriteLine(getValue);
+
+    bool keyExists = await db.KeyExistsAsync("key1");
+    Console.WriteLine(keyExists);
+
+    db.KeyDelete("key1");
+    keyExists = await db.KeyExistsAsync("key1");
+    Console.WriteLine(keyExists);
+
+    var execute = await db.ExecuteAsync("ping");
+    Console.WriteLine(execute);
+
+    var setValue = await db.StringSetAsync("key2", "value2");
+    Console.WriteLine(setValue);
+
+    var getValue2 = await db.StringGetAsync("key2");
+    Console.WriteLine(getValue2);
+}
